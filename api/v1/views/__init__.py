eb89603a17fa4d980e7344ebde91862efc9b0ef8
root <root@TasnimSomo>
2024-04-28 21:42:10 +0400

    restful api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Status of your API"""
 
-from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
 from api.v1.views.index import *
