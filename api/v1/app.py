75d2f0ee7e2936cfb8e50e2c83b4589e31ae53eb
root <root@TasnimSomo>
2024-04-28 21:41:23 +0400

    restful api

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,16 +4,18 @@
 from flask import Flask, render_template, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def app_tear(error):
-     """ calls methods close() """
-     storage.close()
+    """ calls methods close() """
+    storage.close()
+
 
 @app.errorhandler(404)
 def handle_404_error(error):
@@ -21,7 +23,8 @@ def handle_404_error(error):
     error_response = {"error": "Not found"}
     return make_response(jsonify({'error': "Not found"}), 404)
 
+
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
