61f44dc97f984290eb937d4fb5de0ee2ec2990ba
root <root@TasnimSomo>
2024-04-29 10:18:10 +0400

    restful api

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -16,9 +16,8 @@ def get_cities(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    cities = state.cities
-    cities_json = [city.to_dict() for city in cities]
-    return jsonify(cities_json), 200
+    cities_json = [city.to_dict() for city in state.cities]
+    return jsonify(cities_json)
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
@@ -28,7 +27,7 @@ def get_city(city_id):
     if not city:
         abort(404)
     city_json = city.to_dict()
-    return jsonify(city_json), 200
+    return jsonify(city_json)
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
@@ -39,7 +38,7 @@ def delete_city(city_id):
         abort(404)
     storage.delete(city)
     storage.save()
-    return jsonify({}), 200
+    return jsonify({})
 
 
 @app_views.route(
@@ -54,11 +53,11 @@ def create_city(state_id):
     data = request.get_json()
     if 'name' not in data:
         abort(400, 'Missing name')
-    data['state_id'] = state_id
+    data = request.get_json()
     city = City(**data)
+    city.state_id = state.id
     city.save()
-    new_city = city.to_dict()
-    return jsonify(new_city), 201
+    return jsonify(city.to_dict()), 201
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
@@ -67,11 +66,11 @@ def update_city(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    if not request.is_json:
+    if not request.get_json():
         abort(400, 'Not a JSON')
     data = request.get_json()
     for key, value in data.items():
         if key not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(city, key, value)
-    city.save()
-    return jsonify(city.to_dict()), 200
+    storage.save()
+    return jsonify(city.to_dict())
