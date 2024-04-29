2d75de2de66dea3574476d750f4b945fbbea40ac
root <root@TasnimSomo>
2024-04-29 09:32:38 +0400

    restful api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,8 +11,8 @@ from models import storage
 def every_state():
     """ get all by id """
     states = storage.all(State)
-    states_list = [state.to_dict() for state in states.values()]
-    return jsonify(states_list)
+    return jsonify([state.to_dict() for state in states.values()])
+
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def one_state(state_id):
