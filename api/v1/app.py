d8a64490786368d4403e68639a4f806c7a29fa3c
root <root@TasnimSomo>
2024-05-01 09:45:42 +0400

    restful api

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ app.register_blueprint(app_views)
 
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def app_tear(error):
     """ calls methods close() """
